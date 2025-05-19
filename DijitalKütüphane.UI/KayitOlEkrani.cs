ca8c7b6b687c2f859939a59e201a028555a043a2
Berkay Arslan <arslanberkay18@gmail.com>
2025-05-19 21:24:33 +0300

    Kayıt ol ekranı tamamlandı

--- DijitalKütüphane.UI/KayitOlEkrani.cs
+++ DijitalKütüphane.UI/KayitOlEkrani.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using DijitalKütüphane.UI.Context;
+using DijitalKütüphane.UI.Data;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +15,11 @@ namespace DijitalKütüphane.UI
 {
     public partial class KayitOlEkrani : Form
     {
+        private readonly DijitalKutuphaneDbContext _db;
         public KayitOlEkrani()
         {
             InitializeComponent();
+            _db = new DijitalKutuphaneDbContext();
         }
 
         private void KayitOlEkrani_Load(object sender, EventArgs e)
@@ -23,28 +27,65 @@ namespace DijitalKütüphane.UI
             cbUyelikSuresi.Items.AddRange(new string[] { "3 ay", "6 ay", "9 ay", "1 yıl" });
         }
 
+        private int SecilenUyelikSuresi()
+        {
+            int uyelikSuresi = 0;
+            switch (cbUyelikSuresi.SelectedItem)
+            {
+                case "3 ay":
+                    uyelikSuresi = 3;
+                    break;
+                case "6 ay":
+                    uyelikSuresi = 6;
+                    break;
+                case "9 ay":
+                    uyelikSuresi = 9;
+                    break;
+                case "1 yıl":
+                    uyelikSuresi = 12;
+                    break;
 
+            }
+            return uyelikSuresi;
+        }
 
 
 
         private void btnKayitOl_Click(object sender, EventArgs e)
         {
             //Bir tane hata olsa dahi true olup çıkar!
-            if (!txtAd.ValidateTextBox("Ad alanı doldurulmalıdır!") || 
+            if (!txtAd.ValidateTextBox("Ad alanı doldurulmalıdır!") ||
                 !txtSoyad.ValidateTextBox("Soyad alanı doldurulmalıdır!") ||
-                !txtKullaniciAdi.ValidateTextBox("Kullanıcı adı alanı doldurulmalıdır!") || 
-                !txtSifre.ValidateTextBox("Şifre alanı doldurulmalıdır!") || 
-                !txtEmail.ValidateTextBox("Email alanı doldurulmalıdır!") || 
-                !txtUlke.ValidateTextBox("Ülke alanı doldurulmalıdır!") || 
-                !txtSehir.ValidateTextBox("Şehir alanı doldurulmalıdır!") || 
-                !mtxtTelefon.ValidateMaskedTextBox("Telefon alanı doldurulmalıdır!") || 
+                !txtKullaniciAdi.ValidateTextBox("Kullanıcı adı alanı doldurulmalıdır!") ||
+                !txtSifre.ValidateTextBox("Şifre alanı doldurulmalıdır!") ||
+                !txtEmail.ValidateTextBox("Email alanı doldurulmalıdır!") ||
+                !txtUlke.ValidateTextBox("Ülke alanı doldurulmalıdır!") ||
+                !txtSehir.ValidateTextBox("Şehir alanı doldurulmalıdır!") ||
+                !mtxtTelefon.ValidateMaskedTextBox("Telefon alanı doldurulmalıdır!") ||
                 !dtpDogumTarihi.ValidateDateTimePicker("Doğum tarihi ileri bir tarih olamaz!") ||
                 !cbUyelikSuresi.ValidateComboBox("Lütfen bir üyelik süresi seçiniz!"))
             {
                 return;
             }
 
+            Uye uye = new Uye
+            {
+                Ad = txtAd.Text,
+                Soyad = txtSoyad.Text,
+                KullaniciAdi = txtKullaniciAdi.Text,
+                Sifre = txtSifre.Text,
+                DogumTarihi = dtpDogumTarihi.Value.Date,
+                Telefon = mtxtTelefon.Text,
+                Email = txtEmail.Text,
+                Ulke = txtUlke.Text,
+                Sehir = txtSehir.Text,
+                UyelikSuresi = SecilenUyelikSuresi(),
+                Fotograf = txtFotograf.Text,
+            };
 
+            _db.Uyeler.Add(uye);
+            _db.SaveChanges();
+            MessageBox.Show("Kayıt olma işleminiz başarıyla sonuçlandı");
 
         }
 
@@ -55,6 +96,29 @@ namespace DijitalKütüphane.UI
             this.Hide();
         }
 
+        private void chkSifreGoster_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSifreGoster.Checked)
+            {
+                txtSifre.PasswordChar = '*';
+            }
+            else
+            {
+                txtSifre.PasswordChar = '\0';
+            }
+        }
 
+        string fotografDosyaYolu;
+        private void btnFotografSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Resim Dosyaları|*.jpg;,*.jpeg;,*.png;";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fotografDosyaYolu = openFileDialog.FileName;
+                pbFotograf.Image = Image.FromFile(fotografDosyaYolu);
+                txtFotograf.Text = fotografDosyaYolu;
+            }
+        }
     }
 }
